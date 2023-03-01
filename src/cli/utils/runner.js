dd9c21806954409a7665dc818458278bc76508ef
mcmooneyham <matthewcmooneyham@gmail.com>
2023-03-01 11:37:40 -0700

    Display master key in Azure logs for troubleshooting

--- src/cli/utils/runner.js
+++ src/cli/utils/runner.js
@@ -3,9 +3,9 @@ import program from './commander';
 function logStartupOptions(options) {
   for (const key in options) {
     let value = options[key];
-    if (key == 'masterKey') {
-      value = '***REDACTED***';
-    }
+    //if (key == 'masterKey') {
+    //  value = '***REDACTED***';
+    //}
     if (key == 'push' && options.verbose != true) {
       value = '***REDACTED***';
     }
